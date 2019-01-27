21f8c11d62cee0d0c4ad156928585a37634e450b
Vitor Rodrigo Vezani <vitor.vezani@freshcells.de>
2019-01-27 12:45:27 +0100

    Add Spring Boot project

---
+++ src/main/java/br/com/vvezani/springbootrediscache/SpringbootRedisCacheApplication.java
@@ -0,0 +1,14 @@
+package br.com.vvezani.springbootrediscache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRedisCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRedisCacheApplication.class, args);
+	}
+
+}
+
